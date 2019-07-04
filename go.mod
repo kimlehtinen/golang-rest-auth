9bcbc793d93b1f837f9e4ccfc35f622c8f18d816
kim3z <kim.lehtinen@hotmail.com>
2019-07-04 21:03:57 +0300

    Working http server using julienschmidt/httprouter

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kim3z/golang-rest-auth
 
 go 1.12
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/julienschmidt/httprouter v1.2.0
+)
--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
+
+func Home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome home!\n")
+}
+
+func HelloName(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Hello, %s!\n", ps.ByName("name"))
+}
 
 func main() {
 	fmt.Println("Hello, golang-rest-auth")
+
+	router := httprouter.New()
+	router.GET("/", Home)
+	router.GET("/hello/:name", HelloName)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }

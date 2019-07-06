3b37973a46aaaf04aaa44cea29c6c2ceb709c21d
kim3z <kim.lehtinen@hotmail.com>
2019-07-06 20:03:54 +0300

    Removed some unnecessary routes

--- main.go
+++ main.go
@@ -7,32 +7,21 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/kim3z/golang-rest-auth/controllers"
-	"github.com/kim3z/golang-rest-auth/middleware"
 )
 
 func Home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome home!\n")
 }
 
-func Foo(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome bar!\n")
-}
-
-func HelloName(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "Hello, %s!\n", ps.ByName("name"))
-}
-
 func main() {
-	fmt.Println("Hello, golang-rest-auth")
-
 	router := httprouter.New()
 	router.GET("/", Home)
-	router.GET("/hello/:name", middleware.Authenticate(httprouter.Handle(HelloName)))
-	router.GET("/foo", middleware.Authenticate(httprouter.Handle(Foo)))
-
 	router.POST("/api/user/create", controllers.CreateUser)
 	router.POST("/api/user/login", controllers.LoginUser)
 	router.POST("/api/user/forgot-password/:email", controllers.ForgotPassword)
+
+	// Protected route example
+	// router.GET("/foo", middleware.Authenticate(httprouter.Handle(Foo)))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
